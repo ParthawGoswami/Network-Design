0246a2b804e30fbe1729a6179205f524d8c8aec9
Parthaw Goswami <66155526+ParthawGoswami@users.noreply.github.com>
2025-02-08 13:38:12 -0500

    Create udpclient.py

---
+++ Phase 1/Phase 1(a)/udpclient.py
@@ -0,0 +1,14 @@
+# echo-client.py
+
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as clientSocket:
+    print(f"Client connected to server")
+    message = "HELLO"
+    clientSocket.sendto(message.encode(), (HOST, PORT)) # sends the message to the server
+    print(f"Client is sending message...")
+    message, serverAddress = clientSocket.recvfrom(2048) # read server's reply and get address of sender
+print(f"Received from server: {message.decode()}") # print server's reply
